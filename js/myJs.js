8efba93626c722f3d0cca9bd4e9ed3b0a2ad65d2
tranvanmanh1411 <60852466+tranvanmanh1411@users.noreply.github.com>
2020-02-10 01:49:59 +0700

    Add files via upload

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
     // process bar
     setTimeout(function() {
+        firstQuestion();
         $('.spinner').fadeOut();
         $('#preloader').delay(350).fadeOut('slow');
         $('body').delay(350).css({
             'overflow': 'visible'
         });
     }, 600);
-
 })
+
+function firstQuestion(){
+    
+    $('.content').hide();
+    Swal.fire({
+        title: 'He luu cậu!',
+        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        imageUrl: 'img/cuteCat.jpg',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+      }).then(function(){
+        $('.content').show(200);
+      })
+}
 
  // switch button position
  function switchButton() {
+    var audio = new Audio('sound/duck.mp3');
+    audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
     var leftY = $('#yes').css("left");
@@ -20,12 +38,18 @@ $(document).ready(function() {
     $('#no').css("top", topY);
     $('#yes').css("left", leftNo);
     $('#yes').css("top", topNO);
-    console.log('kk');
 }
 // move random button póition
 function moveButton() {
-    var x = Math.random() * 500;
-    var y = Math.random() * 500;
+    var audio = new Audio('sound/Swish1.mp3');
+    audio.play();
+    if (screen.width<=600) {
+        var x = Math.random() * 300;
+        var y = Math.random() * 500;
+    } else{
+        var x = Math.random() * 500;
+        var y = Math.random() * 500;
+    }
     var left = x + 'px';
     var top = y + 'px';
     $('#no').css("left", left);
@@ -41,6 +65,10 @@ $('#no').mousemove(function() {
         moveButton();
     n++;
 });
+$('#no').click(() => {
+    if (screen.width>=900)
+        switchButton();
+})
 
 // generate text in input
 function textGenerate() {
@@ -65,16 +93,18 @@ function textGenerate() {
 
 // show popup
 $('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
     Swal.fire({
         title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
         html: true,
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("../img/iput-bg.jpg")',
+        background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
-              url("../img/giphy2.gif")
+              url("img/giphy2.gif")
               left top
               no-repeat
             `,
@@ -89,10 +119,14 @@ $('#yes').click(function() {
         if (result.value) {
             Swal.fire({
                 width: 900,
-                confirmButtonText: 'Yêuuuuuu <3',
-                background: '#fff url("../img/iput-bg.jpg")',
-                title: 'Biết mà! Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v',
+                confirmButtonText: 'Okiiiii lun <3',
+                background: '#fff url("img/iput-bg.jpg")',
+                title: 'Tớ biết mà ^^ Yêu cậu 300.000',
+                text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
                 confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = 'http://fb.com';
+                  }
             })
         }
     })
